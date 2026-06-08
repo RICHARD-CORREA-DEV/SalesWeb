bf60ad3d8654a422949a50938886509f51bf1e57
RICHARD CORREA <desenvolvedor.rc@gmail.com>
2026-06-08 11:57:35 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 
 public class SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : DbContext(options)
 {
     public DbSet<SalesWebMvc.Models.Department> Department { get; set; } = default!;
+    public required DbSet<Seller> Seller { get; set; } = default!;
+    public required DbSet<SalesRecord> SalesRecord { get; set; } = default!;
 }
